d2d0243d6d97cabd0cf830de1f10877d09bf1ffe
BKRApps <kumarreddy72@gmail.com>
2018-03-28 08:40:59 +0530

    added initial merge commints

--- MergeRebase/Development-2.swift
+++ MergeRebase/Development-2.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+<<<<<<< HEAD
+=======
+class Development2 {
+    func createdDevelopment2(){
+        print("development-2")
+    }
+}
+
+>>>>>>> added initial merge commints
 class Development3 {
     func createdDevelopment3(){
         print("development-3.")
