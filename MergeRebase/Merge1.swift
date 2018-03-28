0426200fa6c3976fe66bdc8f633f3db9bbfcfdff
BKRApps <kumarreddy72@gmail.com>
2018-03-28 08:40:59 +0530

    added initial merge commints

---
+++ MergeRebase/Merge1.swift
@@ -0,0 +1,15 @@
+//
+//  Merge1.swift
+//  MergeRebase
+//
+//  Created by kumar reddy on 28/03/18.
+//  Copyright © 2018 BKRApps. All rights reserved.
+//
+
+import Foundation
+
+class Merge1Initial {
+    func createdMerge1(){
+        print("merge-Initial 1")
+    }
+}
