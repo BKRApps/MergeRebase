eb7905b991fd76b8dd220f7a08f38f099bfde03e
BKRApps <kumarreddy72@gmail.com>
2018-03-28 08:35:03 +0530

    added development-1 & development-2 files

---
+++ MergeRebase/Development-1.swift
@@ -0,0 +1,15 @@
+//
+//  Development-1.swift
+//  MergeRebase
+//
+//  Created by kumar reddy on 28/03/18.
+//  Copyright © 2018 BKRApps. All rights reserved.
+//
+
+import Foundation
+
+class Development1 {
+    func createdDevelopment1(){
+        print("development-1")
+    }
+}
